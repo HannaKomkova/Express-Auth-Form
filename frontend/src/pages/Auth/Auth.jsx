772f928131597585c813fe25002e332b6790e42c
HannaKomkova <nutakomkova@mail.ru>
2026-03-25 22:54:18 +0300

    added post to authorization

--- frontend/src/pages/Auth/Auth.jsx
+++ frontend/src/pages/Auth/Auth.jsx
@@ -1,17 +1,32 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import Style from './authstyle.module.scss'
+import Style from './authstyle.module.scss';
+import { useState } from 'react';
+import axios from 'axios'
 
 function showAuth(){
+
+    const [inputUserEmail, setInputUserEmail] = useState('')
+    const [inputUserPassword, setInputUserPassword] = useState('')
+
+    async function logInUser(){
+        const response = await axios.post('http://localhost:3000/api/authorize', 
+            {
+                email: inputUserEmail,
+                pwd: inputUserPassword
+  })
+        console.log(response.data)
+    }
+
     return(
     <>
         
             <h1>Authorization</h1>
              <div className={Style.container}>
-                <TextField id="outlined-basic" label="Email" variant="outlined" className={Style.input}/>
-                <TextField id="outlined-basic" label="Password" variant="outlined" className={Style.input}/>
+                <TextField id="outlined-basic" label="Email" variant="outlined" className={Style.input} onChange={(e)=> setInputUserEmail(e.target.value)}/>
+                <TextField id="outlined-basic" label="Password" variant="outlined" className={Style.input} onChange={(e)=> setInputUserPassword(e.target.value)}/>
 
-                <Button variant="contained" className={Style.button}>Send</Button>
+                <Button variant="contained" className={Style.button} onClick={logInUser}>Log in</Button>
             </div>
        
     </>
