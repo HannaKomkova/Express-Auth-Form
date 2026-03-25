a5a873f40997deb0ffa9c2818a7bbb9f2c1624cb
HannaKomkova <nutakomkova@mail.ru>
2026-03-25 22:19:16 +0300

    added UI to register

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,8 +1,20 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Style from './style.module.scss'
+
 function showRegister(){
     return(
     <>
         
             <h1>Registration</h1>
+            <div className={Style.container}>
+                <TextField id="outlined-basic" label="User name" variant="outlined" className={Style.input}/>
+                <TextField id="outlined-basic" label="Email" variant="outlined" className={Style.input}/>
+                <TextField id="outlined-basic" label="Phone number" variant="outlined" className={Style.input}/>
+                <TextField id="outlined-basic" label="Password" variant="outlined" className={Style.input}/>
+
+                <Button variant="contained" className={Style.button}>Send</Button>
+            </div>
     
     </>
 )}
