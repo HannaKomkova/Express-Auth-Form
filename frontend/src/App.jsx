05c2bd580a519364534067d44117a3c1cbc391a2
HannaKomkova <nutakomkova@mail.ru>
2026-03-25 22:01:28 +0300

    added react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Auth from './pages/Auth/Auth'
+import Register from './pages/Register/Register'
 
 function App() {
 
   return (
     <>
+    <Routes>
+      <Route path='/' element={<Auth/>}/>
+      <Route path='/reg' element={<Register/>}/>
+    </Routes>
     </>
   )
 }
